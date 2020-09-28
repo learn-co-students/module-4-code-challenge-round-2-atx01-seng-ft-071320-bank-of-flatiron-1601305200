1974fb08519b8c85b537861227387c6befbfcc25
Christorres392 <christorres09@gmail.com>
2020-09-28 11:01:16 -0600

    submission

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,57 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+  
+  componentDidMount() {
+    this.fetchTransactions()
+  }
+  
+  fetchTransactions(){
+    fetch("http://localhost:6001/transactions")
+    .then(res => res.json())
+    .then(transactions => this.setState({transactions}))
+  }
+
+  transactionForm = (transaction) => {
+    fetch("http://localhost:6001/transactions", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify ({
+        "date": transaction.date,
+      "description": transaction.description,
+      "category": transaction.category,
+      "amount": transaction.amount
+      })
+    })
+    .then(res => res.json())
+    .then(() => this.fetchTransactions())
+    
+  }
+
+  transactionSearch = (search) => {
+    console.log(search)
+     this.fetchTransactions()
+     setTimeout(() => {
+       let newArray = this.state.transactions.filter(transaction => transaction.description.startsWith(search))
+        this.setState({
+         transactions: newArray
+      })
+     }, 50)
+  }
+
   render() {
-    return (
+    return ( 
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search transactionSearch={this.transactionSearch}/>
+        <AddTransactionForm transactionForm={this.transactionForm}/>
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,23 @@
 import React from "react";
 
-const Search = () => {
+const Search = (props) => { console.log(props)
+
+  // handleChange = (event) => {
+  //   props.transactionSearch(event.target.value)
+  // }
+  
+  let handleChange = (event) => {
+    props.transactionSearch(event.target.value)
+  }
+  
+
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
+        onChange={ handleChange
+          }
       />
       <i className="circular search link icon"></i>
     </div>
